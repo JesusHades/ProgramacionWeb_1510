95ada42321034b877f25ea353a6d652ace1fa858
JesusHades <jesusmanriquez56@aragon.unam.mx>
2023-11-15 20:28:17 -0600

    Se agrega funcion para validar entrada de numeros en un campo del formulario

--- ProyectoFInal/Formulario.php
+++ ProyectoFInal/Formulario.php
@@ -5,9 +5,28 @@ session_start();
 <!DOCTYPE html>
 <html lang = "en">
 
+<head>
+    <title>Registrar Usuario</title>
+    <meta charset = "utf-8">
+</head>
 
+<script>
 
+    function valida(e) {
+        tecla = (document.all) ? e.keyCode : e.which;
 
+        //Tecla de retroceso siempre permitida
+        if(tecla == 8){
+            return true;
+        }
+
+        //entrada, solo acepta numeros
+        entrada =/[0-9]/;
+        tecla_final = String.fromCharCode(tecla);
+        return entrada.test(tecla_final);
+    }
+
+</script>
 
 
 

31a1936361c0e3ea60616105d62cd5088c3fa523
JesusHades <jesusmanriquez56@aragon.unam.mx>
2023-11-24 23:44:09 -0600

    Se agrega la ejecucon del query

--- ProyectoFInal/EliminarUsuario_Funcion.php
+++ ProyectoFInal/EliminarUsuario_Funcion.php
@@ -4,6 +4,9 @@ require "conexion.php";
 mysqli_set_charset($conexion,'utf8');
 
 $registroEliminado =$_POST['num_cuenta'];
-$consulta="DELETE FROM persona WHERE num_cuenta = ";
+$consulta="DELETE FROM usuarios WHERE num_cuenta = ";
+
+mysqli_query($conexion, $consulta . $registroEliminado);
+mysqli_close($conexion);
 
 ?>

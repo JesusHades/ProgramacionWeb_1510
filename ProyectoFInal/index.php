75cffda6e729452cc7eb0274d404580ed4939140
JesusHades <jesusmanriquez56@aragon.unam.mx>
2023-11-24 22:46:46 -0600

    Se agrega el query con la informacion almacenada en variables

--- ProyectoFInal/index.php
+++ ProyectoFInal/index.php
@@ -4,6 +4,16 @@ require "conexion.php";
 mysqli_set_charset($conexion,'utf8');
 
 
+//genear el query
+$consulta_sql="SELECT * FROM usuario";
+
+
+//mandar el query por medio de la conexion y almacenaremos el resultado en una variable
+$resultado = $conexion->query($consulta_sql);
+
+
+// Retorna el numero de filas del resultado.
+$count = mysqli_num_rows($resultado);
 
 
 
